ad88e55c4c048b8a8e88bc63d7442b712d6e9dcf
LibrarySteve <sgarrison@lexpublib.org>
2019-05-03 01:18:09 -0400

    Fixed more errors
    fixed more poor python formatting

--- pifightercar.py
+++ pifightercar.py
@@ -5,11 +5,11 @@ BLYNK_AUTH = 'ADD_API_KEY_HERE'
 
 blynk = blynklib.Blynk(BLYNK_AUTH, server='SERVER_IP_HERE', port='8080')
 
-momo1 = adafruit.dc_motor_1
-momo2 = adafruit.dc_motor_2
+momo1 = crickit.dc_motor_1
+momo2 = crickit.dc_motor_2
 
-@bynk.handle_event('write V0')
-def go_forward{pin, value):
+@blynk.handle_event('write V0')
+def go_forward(pin, value):
     if int(value[0]) == 1:
         momo1.throttle = 1
         momo2.throttle = 1
@@ -17,7 +17,7 @@ def go_forward{pin, value):
         momo1.throttle = 0
         momo2.throttle = 0
 
-@bynk.handle_event('write V1')
+@blynk.handle_event('write V1')
 def go_back(pin, value):
     if int(value[0]) == 1:
         momo1.throttle = -1
@@ -26,7 +26,7 @@ def go_back(pin, value):
         momo1.throttle = 0
         momo2.throttle = 0
 
-@bynk.handle_event('write V2')
+@blynk.handle_event('write V2')
 def turn_right(pin, value):
     if int(value[0]) == 1:
         momo1.throttle = -1
@@ -35,7 +35,7 @@ def turn_right(pin, value):
         momo1.throttle = 0
         momo2.throttle = 0
 
-@bynk.handle_event('write V3')
+@blynk.handle_event('write V3')
 def turn_left(pin, value):
     if int(value[0]) == 1:
         momo1.throttle = 1
