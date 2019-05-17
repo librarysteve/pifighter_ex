9c81df3e32857ee78fc1b49b9b751241d5cfa25c
LibrarySteve <sgarrison@lexpublib.org>
2019-05-17 13:53:59 -0400

    Create Test_Connection.py
    Added a non hardware test just for connection testing.

---
+++ Test_Connection.py
@@ -0,0 +1,16 @@
+import blynklib
+
+#Obtained from the app or server admin page
+BLYNK_AUTH = 'authkeyhere'
+
+#initialize Blynk in this script. The port for a local instance is 8080
+blynk = blynklib.Blynk(BLYNK_AUTH, server='server_ip_address', port='8080')
+
+@blynk.handle_event('write V0')
+def when_button_is_pressed(pin, value):
+    msg = "Virtual Pin Number {0} Value: {1}".format(pin, value)
+    print(msg)
+
+# Run forever or until keyboard interrupt. 
+while True:
+    blynk.run()
